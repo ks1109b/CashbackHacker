9c51533aabb64ba6ab0f6004f220a8d645bde633
Kseniia Bloshkina <ksenia119@hotmail.com>
2021-02-16 18:43:19 +0300

    Added JUnit Jupiter

---
+++ src/test/java/ru/netology/service/CashbackHackServiceJUnit5Test.java
@@ -0,0 +1,37 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceJUnit5Test {
+    private CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldBuyIf1000() {
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldBuyIfUnder1000() {
+        int actual = service.remain(500);
+        int expected = 500;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldBuyIfOver1000() {
+        int actual = service.remain(1500);
+        int expected = 500;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldBuyIf0() {
+        int actual = service.remain(0);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+}
