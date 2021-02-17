6c4c697998a34ef06207774f760b45de31631da7
Kseniia Bloshkina <ksenia119@hotmail.com>
2021-02-17 12:36:40 +0300

    Edited imports & CI

--- src/test/java/ru/netology/service/CashbackHackServiceJUnit5Test.java
+++ src/test/java/ru/netology/service/CashbackHackServiceJUnit5Test.java
@@ -1,8 +1,8 @@
 package ru.netology.service;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CashbackHackServiceJUnit5Test {
     private CashbackHackService service = new CashbackHackService();
